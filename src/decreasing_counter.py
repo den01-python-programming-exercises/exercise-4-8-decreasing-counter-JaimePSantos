300ca17ce7e67404c069dee8ff555f0dddc9aa53
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-24 11:18:48 +0000

    solution

--- src/decreasing_counter.py
+++ src/decreasing_counter.py
@@ -1,12 +1,21 @@
 class DecreasingCounter:
+  value = 0
+  def __init__(self,initial_value):
+    self.value = initial_value
 
-    def __init__(self,initial_value):
-        self.value = initial_value
+  def print_value(self):
+    print("value: " + str(self.value))
 
-    def print_value(self):
-        print("value: " + str(self.value))
-
-    def decrement(self):
+  def reset(self):
+    self.value = 0
+    return self.value
+  
+  def decrement(self):
+    self.value-=1
+    if(self.value<0):
+      self.reset()
+      
+    return self.value
         # write the method implementation here
         # the aim is to decrement the value of the counter by one
 
--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
+from decreasing_counter import DecreasingCounter
+ 
 def main():
-    #write your code below this line
-
+    counter = DecreasingCounter(10)
+ 
+    counter.print_value()
+ 
+    counter.decrement()
+    counter.print_value()
+ 
+    counter.decrement()
+    counter.print_value()
+    
 if __name__ == '__main__':
     main()
